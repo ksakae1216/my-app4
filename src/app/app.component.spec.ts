c40b668e957d7bb99a32b7f34bbbd91dafe6926f
kotaro_sakae <ksakae1216@gmail.com>
2019-05-30 21:31:06 +0900

    test add

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app4'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-app4');
-  });
-
 });
